fffd65e292a8c0408eb3dafc685a2ee87659dfd4
Luisito Programa <devluisitoprograma@gmail.com>
2025-04-13 12:40:57 -0500

    v2.9.91
    Sync Local Server v1

--- windowsState.php
+++ windowsState.php
@@ -1,6 +1,21 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
-$setWindows = file_get_contents("windowsState.txt");
-echo $setWindows;
+header("Content-Type: text/html; charset=utf-8");
+$setWindowsRaw = file_get_contents("windowsState.txt");
+$dataForJs = json_decode($setWindowsRaw, true);
 ?>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Windows State</title>
+  </head>
+  <body>
+    <script>
+      (function(){
+        var data = <?php echo json_encode($dataForJs); ?>;
+        window.parent.postMessage(data, "*");
+      })();
+    </script>
+    <p>Procesando...</p>
+  </body>
+</html>
