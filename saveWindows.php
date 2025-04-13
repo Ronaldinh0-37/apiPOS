fd68b38399895aee2ef6f3d7d34862600fa86587
Luisito Programa <devluisitoprograma@gmail.com>
2025-04-12 20:50:03 -0500

    v2.9.89
    Windows Server Local Successfully !

---
+++ saveWindows.php
@@ -0,0 +1,30 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+if(isset($_POST['setWindows'])){
+$setWindowsJson = $_POST['setWindows'];
+$filePath = __DIR__ . '/windowsState.txt';
+if(file_put_contents($filePath, $setWindowsJson) !== false){
+echo json_encode([
+"success" => true, 
+"message" => "Successfully!"
+]);
+echo "<script>window.close();</script>";
+} else {
+echo json_encode([
+"success" => false, 
+"message" => "Data Save Error."
+]);
+}
+} else {
+echo json_encode([
+"success" => false, 
+"message" => "Param Error."
+]);
+}
+} else {
+echo json_encode([
+"success" => false, 
+"message" => "Method Error."
+]);
+}
+?>
